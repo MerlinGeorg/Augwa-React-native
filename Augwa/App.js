9cf69fb0fa6403d6f08f0df50a2a75d9ab3f27af
Qianhui <helenyu301@gmail.com>
2025-02-06 22:05:11 -0500

    add login screen

--- Augwa/App.js
+++ Augwa/App.js
@@ -3,13 +3,15 @@ import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import SignupScreen from './screens/SignupScreen';
+import LogInScreen from './screens/LogInScreen';
 
 export default function App() {
 
   const MyStack = createStackNavigator();
 
   return (
-     <SignupScreen />
+    <LogInScreen />
+    // <SignupScreen />
     // <NavigationContainer>
     //   <MyStack.Screen name="signup" component={SignupScreen} />
     // </NavigationContainer>
