a5431bdc7741111c81a7a8f635b7eedbfcb897c9
MerlinGeorg <merlingeo96@gmail.com>
2025-02-07 23:07:41 -0500

    test

--- Augwa/screens/LoginScreen.js
+++ Augwa/screens/LoginScreen.js
@@ -1,6 +1,5 @@
-
 import React, { useState } from 'react';
-import { Image, View, StyleSheet, TextInput, Button, Text, TouchableOpacity} from 'react-native';
+import { Image, View, StyleSheet, TextInput, Button, Text } from 'react-native';
 const LogInScreen = (props) =>{
     const [domain, setDomain] = useState('')
     const [userName, setUserName] = useState('') // set user name
@@ -16,32 +15,19 @@ const LogInScreen = (props) =>{
     }
     return(
         <View style = {marginTop=200}>
-          
-          <Text style = {styles.textTitleStyle}>Welcome To Augwa</Text>
-          <TextInput style = {styles.inputView}value = {userName} onChangeText={setUserName}
-              placeholder='Domain: '/>
-          <TextInput style = {styles.inputView}value = {userName} onChangeText={setUserName}
-              placeholder='Username: '/>
-          <TextInput style = {styles.inputView}value = {userName} onChangeText={setUserName}
-              placeholder='Password: '/>
-          <TouchableOpacity style={styles.btnPsw} >
-              <Text style={styles.bluBtntext}>Forgot Password ?</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={{marginTop: 60, width: 340, height: 45, borderRadius: 8,
-              alignSelf: "center", backgroundColor: "#2D4059", justifyContent: "center",
-              alignItems: "center" }} >
-              <Text style={{textAlign: "center", padding: "auto",
-                fontSize: 23, color: '#ffffff'}}>SIGN IN</Text>
-          </TouchableOpacity>
-          <View style = {styles.signupView}>
-            <Text style = {{fontSize: 17, color: '5F5F5F'}}>Don't have an account?</Text>
-            <TouchableOpacity >
-              <Text style={styles.bluBtntext}>  Sign up</Text>
-            </TouchableOpacity>
-
-          </View>
-          
-
+            <Image style={styles.imageStyle} source={require('.../assets/images/app_log.svga')}></Image>
+            <View  style = {styles. viewStyle}>
+              <Text style = {styles.textStyle}>Please Enter Your User Name</Text>
+              <TextInput style = {styles.inputView}value = {userName} onChangeText={setUserName}
+              placeholder='Enter user name:'/>
+            </View>
+            <View style = {styles. viewStyle}>
+              <Text style = {styles.textStyle}>Please Enter Your password</Text>
+              <TextInput style = {styles.inputView}value = {password} onChangeText={setPassword}
+              secureTextEntry = {true}
+              placeholder='Enter password:'/>
+            </View>
+            <Button title='Log In' onPress={checkLogin}></Button>
            
         </View>
     )   
@@ -54,40 +40,22 @@ const styles = StyleSheet.create({
       justifyContent:'space-between',
       margin: 15,
       padding: 10},
-    textTitleStyle: {
+    textStyle : {
       marginTop: 15,
-      fontSize: 35,
-      fontWeight: "bold",
-      alignSelf: "center"
-  },
-    inputView: {
-      height: 45,
-      width: 360,
-      borderWidth: 0.5,
-      borderRadius: 8,
       fontSize: 20,
       fontWeight: "bold",
-      marginTop:5,
-      alignSelf: 'center'
+      alignContent: "center"
+  },
+    inputView: {
+      borderWidth: 1,
+      borderRadius: 5,
+      fontSize: 20,
+      fontWeight: "bold",
     },
     imageStyle: {
       alignSelf:"center",
       width: 50,
       height: 50
-  },
-  btnPsw:{
-    marginLeft: 240,
-    marginTop: 12
-  },
-  bluBtntext:{
-    fontSize: 17,
-    color: '#177de1'
-  },
-  signupView:{
-    marginTop: 60,
-    flexDirection: 'row',
-    alignSelf:'center'
-    
   }
   })
 export default LogInScreen
