c5bf6fa6e830580cd423c3874eba10c84a03513a
Saima <bareensaima2512@gmail.com>
2025-02-11 04:23:55 -0500

    Implementing API function to get bookings

--- Augwa/components/Schedule.js
+++ Augwa/components/Schedule.js
@@ -8,3 +8,44 @@ const api = axios.create({
         'X_domain': X_DOMAIN
     }
 });
+
+export const getBooking = async () => {
+    try {
+        console.log("Fetching Bookings...");
+        const response = await api.get('/Booking');
+        console.log("Booking fetched");
+        console.log("Response Status: ", response.status);
+        return {
+            success: true,
+            data: response.data
+        };
+    } catch (error) {
+        console.error("Error fetching", error);
+        if (error.response) {
+            const { status, data } = error.response;
+            switch(status) {
+                case 400:
+                    return {
+                        success: false,
+                        error: {
+                            message: data.message|| 'Bad Request',
+                            code: 'BAD_REQUEST'
+                        }
+                    };
+                case 401: 
+                    return {
+                        success: false,
+                        error: {
+                            message: data.message|| 'Unauthorized',
+                            code: 'UNAUTHORIZED'
+                        }
+                }
+                default:
+                        return {
+                            success: false,
+                            error: { message: data.message || 'Error fetching bookings', code: 'API_ERROR' }
+                        };
+            }
+        }
+    }
+}
