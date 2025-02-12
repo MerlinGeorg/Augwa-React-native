2ebc936e290d542ddafcbcae58a094c03cf87220
Saima-Mirza <s1663@my.centennialcollege.ca>
2025-02-11 23:00:03 -0500

    Design Schedule screen (#29)
    * Initial code to check
    
    * Added SearchBar component
    
    * Imported ScheduleScreen in App.js
    
    * Designed ScheduleScreen with mock data
    
    * Implementing API function to get bookings
    
    * Update
    
    * Design schedule
    
    * Updated design of searchbar
    
    * Designing scshedule screen complete
    
    ---------
    
    Co-authored-by: Saima <bareensaima2512@gmail.com>

--- Augwa/screens/ScheduleScreen.js
+++ Augwa/screens/ScheduleScreen.js
@@ -4,8 +4,21 @@ import { ScrollView } from 'react-native-gesture-handler';
 import SearchBar from "../components/SearchBar";
 import { getBooking } from "../components/Schedule";
 import { augwaBlue,dashboardArea } from "../assets/styles/color";
-import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import { Ionicons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+const Tabbar = ({ tabName }) => {
+  switch (tabName) {
+    case "Past":
+      return <Text>Past</Text>;
+    case "Today":
+      return <Text>Today</Text>;
+    case "Future":
+      return <Text>Future</Text>;
+    default: 
+      return <Text>Today</Text>
+  }
+};
 
 
 const ScheduleScreen = (props) => {
@@ -14,19 +27,37 @@ const ScheduleScreen = (props) => {
     const [search, setSearch] = useState('')
     const [filtersearch, setFilterSearch] = useState([])
 
+    const [selectedTab, setSelectedTab] = useState("Today");
+
+    const tab = createBottomTabNavigator();
+
 
 return (
     <View style = {[styles.viewStyle]}>
         <View style = {{ backgroundColor: augwaBlue, marginTop:40}}>
             <Text style = {[styles.Title]}>Schedule</Text>
         </View>
-        {/* Schedule area */}
         <View style={styles.dashboardAreaStyle}>
+
+        <View style={styles.tabNavigation}>
+                    <TouchableOpacity onPress={() => setSelectedTab("Past")}>
+                       <Text style={selectedTab === "Past" ? styles.selectedTab : styles.tabText}>Past</Text> 
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => setSelectedTab("Today")}>
+                        <Text style={selectedTab === "Today" ? styles.selectedTab : styles.tabText}>Today</Text> 
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => setSelectedTab("Future")}>
+                        <Text style={selectedTab === "Future" ? styles.selectedTab : styles.tabText}>Future</Text> 
+                    </TouchableOpacity>
+                </View>
+
+{/* Searcbar and menu options */}
           <View style = {styles.row}>
-          <FontAwesome6 name="sliders" />
+            <TouchableOpacity>
+            <Ionicons name="options-outline" style = {styles.iconStyle}/>
+            </TouchableOpacity>
           <SearchBar/>
           </View>
-        <Text style ={[styles.Content]}>Today's Schedule</Text>
         </View>
     </View>
    
@@ -55,14 +86,36 @@ const styles = StyleSheet.create({
      Content: {
         fontSize: 15,
         marginLeft: 10,
+        marginTop: 10,
         fontWeight: 'bold',
         color: '#000'
       },
       row: {
         flexDirection: 'row',
-        justifyContent: 'space-between',
         alignItems: 'center',
+       
       },
+      iconStyle: {
+        color: '#000',
+        fontSize: 30,
+        marginTop:10,
+        alignSelf: 'center',
+        marginHorizontal: 15,
+    },
+    tabNavigation: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      paddingVertical: 10,
+  },
+  tabText: {
+      fontSize: 16,
+      color: 'grey',
+  },
+  selectedTab: {
+      fontSize: 16,
+      fontWeight: 'bold',
+      color: '#177de1', 
+  },
 })
 
 export default ScheduleScreen;
