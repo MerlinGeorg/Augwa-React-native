a34cc7ca0fb00ed5132282d1e13539d23e1e8240
MerlinGeorg <merlingeo96@gmail.com>
2025-02-28 06:00:45 -0500

    notes image uploading with camera, notes content clipping

--- Augwa/screens/ScheduleDetailScreen.js
+++ Augwa/screens/ScheduleDetailScreen.js
@@ -1,12 +1,28 @@
 import React, { useEffect, useState, useContext } from "react";
-import { TouchableOpacity, Text, View, StyleSheet, ScrollView, Image, Alert, ActivityIndicator, Modal, TextInput } from "react-native";
+import {
+  TouchableOpacity,
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  Image,
+  Alert,
+  ActivityIndicator,
+  Modal,
+  TextInput,
+  Platform,
+  SafeAreaView
+} from "react-native";
 import { augwaBlue, dashboardArea } from "../assets/styles/color";
-import { Ionicons } from '@expo/vector-icons';
-import { AuthContext } from '../src/context/AuthContext';
+import { Ionicons } from "@expo/vector-icons";
+import { AuthContext } from "../src/context/AuthContext";
 import axios from "axios";
 import { API_BASEPATH_DEV, X_DOMAIN } from "@env";
-import { launchImageLibrary } from "react-native-image-picker";
-import { openMapsWithDirections} from "../components/NativeMap";
+//import * as ImagePicker from "expo-image-picker";
+import { openMapsWithDirections } from "../components/NativeMap";
+import { ExpandableNote } from "../components/ExpandableNote";
+import { CameraImagePicker } from "../components/CameraImagePicker";
+import CustomAlert from "../components/CustomAlert";
 
 const ScheduleDetailScreen = ({ route }) => {
   const { authToken } = useContext(AuthContext);
@@ -29,13 +45,12 @@ const ScheduleDetailScreen = ({ route }) => {
       const response = await api.get(`/Booking/${jobId}`, {
         headers: {
           Authorization: `Bearer ${authToken}`,
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        }
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
       });
-console.log("jobId: ", jobId)
+     // console.log("jobId: ", jobId);
       setJob(response.data);
-      
     } catch (error) {
       console.error("Failed to fetch updated job status:", error);
     } finally {
@@ -48,7 +63,13 @@ console.log("jobId: ", jobId)
   }, []);
 
   if (loading) {
-    return <ActivityIndicator size="large" color="#177de1" style={{ marginTop: 50 }} />;
+    return (
+      <ActivityIndicator
+        size="large"
+        color="#177de1"
+        style={{ marginTop: 50 }}
+      />
+    );
   }
 
   // Make sure the job exists
@@ -69,7 +90,9 @@ console.log("jobId: ", jobId)
   // Format time
   const formatTime = (start, end) => {
     const timeOptions = { hour: "2-digit", minute: "2-digit", hour12: true };
-    return `${new Date(start).toLocaleTimeString([], timeOptions)} - ${new Date(end).toLocaleTimeString([], timeOptions)}`;
+    return `${new Date(start).toLocaleTimeString([], timeOptions)} - ${new Date(
+      end
+    ).toLocaleTimeString([], timeOptions)}`;
   };
 
   const renderActionButton = () => {
@@ -79,19 +102,28 @@ console.log("jobId: ", jobId)
 
     if (job.status === "Scheduled") {
       return (
-        <TouchableOpacity style={styles.startButton} onPress={() => handleAction("start")}>
+        <TouchableOpacity
+          style={styles.startButton}
+          onPress={() => handleAction("start")}
+        >
           <Text style={styles.startButtonText}>Start</Text>
         </TouchableOpacity>
       );
     } else if (job.status === "InProgress") {
-      console.log("job: ",job)
-      
+      //  console.log("job: ", job.items);
+
       return (
         <>
-          <TouchableOpacity style={styles.completeButton} onPress={() => handleAction("complete")}>
+          <TouchableOpacity
+            style={styles.completeButton}
+            onPress={() => handleAction("complete")}
+          >
             <Text style={styles.completeButtonText}>Complete</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.cancelButton} onPress={() => handleAction("cancel")}>
+          <TouchableOpacity
+            style={styles.cancelButton}
+            onPress={() => handleAction("cancel")}
+          >
             <Text style={styles.cancelButtonText}>Cancel</Text>
           </TouchableOpacity>
         </>
@@ -103,33 +135,33 @@ console.log("jobId: ", jobId)
 
   const handleAction = async (actionType) => {
     if (actionType === "cancel") {
-      Alert.alert(
-        "Cancel Job",
-        "Are you sure you want to cancel this job?",
-        [
-          { text: "No", style: "cancel" },
-          {
-            text: "Yes, Cancel",
-            style: "destructive",
-            onPress: async () => {
-              try {
-                const response = await api.post(`/Booking/${job.id}/Stop`, {}, {
+      Alert.alert("Cancel Job", "Are you sure you want to cancel this job?", [
+        { text: "No", style: "cancel" },
+        {
+          text: "Yes, Cancel",
+          style: "destructive",
+          onPress: async () => {
+            try {
+              const response = await api.post(
+                `/Booking/${job.id}/Stop`,
+                {},
+                {
                   headers: {
                     Authorization: `Bearer ${authToken}`,
-                  }
-                });
-
-                if (response.status === 204) {
-                  console.log("Job successfully canceled");
-                  fetchUpdatedJob();
+                  },
                 }
-              } catch (error) {
-                console.error("Failed to cancel job:", error);
+              );
+
+              if (response.status === 204) {
+                console.log("Job successfully canceled");
+                fetchUpdatedJob();
               }
-            },
+            } catch (error) {
+              console.error("Failed to cancel job:", error);
+            }
           },
-        ]
-      );
+        },
+      ]);
       return;
     }
 
@@ -142,11 +174,15 @@ console.log("jobId: ", jobId)
         endpoint = `/Booking/${job.id}/Complete`;
       }
 
-      const response = await api.post(endpoint, {}, {
-        headers: {
-          Authorization: `Bearer ${authToken}`,
+      const response = await api.post(
+        endpoint,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
         }
-      });
+      );
 
       if (response.status === 204) {
         console.log(`Successfully updated job status via ${actionType}`);
@@ -157,20 +193,9 @@ console.log("jobId: ", jobId)
     }
   };
 
-  // Pick the image
-  const pickImage = () => {
-    launchImageLibrary({ mediaType: 'photo', includeBase64: true }, (response) => {
-      if (response.didCancel) return;
-      if (response.errorMessage) {
-        console.error("Image Picker Error:", response.errorMessage);
-        return;
-      }
-
-      if (response.assets && response.assets.length > 0) {
-        setImageData(response.assets[0].base64); // Store base64 image
-      }
-    });
-  };
+  const handleCaptureImage = async () => {
+    CameraImagePicker(setImageData);
+};
 
   // Add notes
   const handleAddNote = async () => {
@@ -185,14 +210,20 @@ console.log("jobId: ", jobId)
         imageData: imageData || null,
         visibility: "Private",
       };
+    //  console.log("payload", payload);
 
       const response = await api.post(`/Booking/${jobId}/Notes`, payload, {
         headers: {
           Authorization: `Bearer ${authToken}`,
-        }
+        },
       });
 
       if (response.status === 200) {
+        CustomAlert({
+          title: "success",
+          message: "Note added successfully!",
+          onOk: () => {}
+        })
         console.log("Note added successfully!");
         // Close the modal and reset the note and image
         setModalVisible(false);
@@ -210,29 +241,38 @@ console.log("jobId: ", jobId)
   //pass data to map
   const handleGetDirections = () => {
     openMapsWithDirections({
-       latitude: job.latitude,
-       longitude: job.longitude,
+      latitude: job.latitude,
+      longitude: job.longitude,
       // address: job.address
-    })
-  }
-
+    });
+  };
 
   return (
-    <View style={styles.viewStyle}>
+    <SafeAreaView style={styles.viewStyle}>
       <View style={{ backgroundColor: augwaBlue, marginTop: 40 }}>
         <Text style={styles.Title}>Job Details</Text>
       </View>
 
       <View style={styles.dashboardAreaStyle}>
-        <ScrollView style={styles.container}>
-
+        <ScrollView style={styles.container} contentContainerStyle = { styles.scrollContainer}>
           {/*----- Status & Start Button -----*/}
           <View style={styles.statusContainer}>
-            <Text style={styles.statusText}>{job.status == "InProgress" ? "In Progress" : job.status}</Text>
-            <View style={styles.buttonRow}>
-              {renderActionButton()}
-            </View>
+            <Text style={styles.statusText}>
+              {job.status == "InProgress" ? "In Progress" : job.status}
+            </Text>
+            <View style={styles.buttonRow}>{renderActionButton()}</View>
           </View>
+
+          {/*----- Status & Start Button -----*/}
+          {/* <View style={styles.card}>
+            <View style={styles.row}>
+              <Ionicons name="time-outline" size={24} color="#333" />
+            </View>
+            <View style={styles.row}>
+               <Text style={styles.timeText}>{formatTime(time)}</Text>
+          <Text style={styles.dateText}>{displayDate}</Text> 
+            </View>
+          </View> */}
 
           {/*----- Job Date and Time -----*/}
           <View style={styles.card}>
@@ -243,7 +283,9 @@ console.log("jobId: ", jobId)
 
             <View style={styles.row}>
               <Text style={styles.label}>Time:</Text>
-              <Text style={styles.value}>{formatTime(job.startDate, job.endDate)}</Text>
+              <Text style={styles.value}>
+                {formatTime(job.startDate, job.endDate)}
+              </Text>
             </View>
           </View>
 
@@ -251,7 +293,9 @@ console.log("jobId: ", jobId)
           <View style={styles.card}>
             <View style={styles.row}>
               <Text style={styles.label}>Client:</Text>
-              <Text style={styles.value}>{job.client.fullName} ({job.client.companyName})</Text>
+              <Text style={styles.value}>
+                {job.client.fullName} ({job.client.companyName})
+              </Text>
             </View>
           </View>
 
@@ -264,8 +308,38 @@ console.log("jobId: ", jobId)
             </View>
             {/* <Image source={{ uri: `https://maps.googleapis.com/maps/api/staticmap?center=${job.latitude},${job.longitude}&zoom=15&size=300x150&maptype=roadmap&key=YOUR_GOOGLE_MAPS_API_KEY` }} style={styles.mapImage} /> */}
             <TouchableOpacity onPress={handleGetDirections}>
-              <Text style={styles.getDirections}>Get Directions <Ionicons name="open-outline" size={16} color="blue" /></Text>
+              <Text style={styles.getDirections}>
+                Get Directions{" "}
+                <Ionicons name="open-outline" size={16} color="blue" />
+              </Text>
             </TouchableOpacity>
+          </View>
+
+          {/*----- Job details -----*/}
+          <View style={styles.card}>
+            <Text style={styles.label}>Job Details:</Text>
+            {job.items && job.items.length > 0 ? (
+              job.items.map((item, index) => (
+                <View>
+                  <View style={styles.row}>
+                    <Text style={styles.jobLabel}>Job</Text>
+                    <Text style={styles.jobValue}>{item.name}</Text>
+                  </View>
+                  <View style={styles.row}>
+                    <Text style={styles.jobLabel}>Type</Text>
+                    <Text style={styles.jobValue}>{item.itemType}</Text>
+                  </View>
+                  <View style={styles.row}>
+                    <Text style={styles.jobLabel}>Description</Text>
+                    <Text style={styles.jobValue}>
+                      {item.description || "N/A"}
+                    </Text>
+                  </View>
+                </View>
+              ))
+            ) : (
+              <Text>No job detials available</Text>
+            )}
           </View>
 
           {/*----- Notes Section -----*/}
@@ -277,24 +351,45 @@ console.log("jobId: ", jobId)
               </TouchableOpacity>
             </View>
 
-            {job.notes && job.notes.length > 0 ? (
+           
+             {job.notes && job.notes.length > 0 ? (
               job.notes.map((note, index) => (
-                <View key={index} style={styles.note}>
-                  <View style={styles.row}>
-                    <Text style={styles.noteAuthor}>{note.author}</Text>
-                    <Text style={styles.noteDate}>{new Date(note.dateCreated).toLocaleString()}</Text>
-                  </View>
-                  <Text style={styles.noteText}>{note.content}</Text>
-                  {note.imageData && <Image source={{ uri: `data:image/png;base64,${note.imageData}` }} style={styles.noteImage} />}
-                </View>
-              ))
+
+                 <ExpandableNote key={index} note={note} />
+                
+                // <View key={index} style={styles.note}>
+                //   <View style={styles.row}>
+                //     <Text style={styles.noteAuthor}>{note.author}</Text>
+                //     <Text style={styles.noteDate}>
+                //       {new Date(note.dateCreated).toLocaleString()}
+                //     </Text>
+                //   </View>
+                //   <Text style={styles.noteText}>{note.content}</Text>
+                //   { note.imageUrl ? (
+                     
+                //         <Image
+                //           source={{ uri: note.imageUrl }}
+                //           style={styles.noteImage}
+                //         />
+                      
+                //   ) : null }
+                  
+                // </View>
+              )) 
             ) : (
               <Text>No notes available</Text>
             )}
+            
+            
           </View>
 
           {/*----- Modal for Adding Notes -----*/}
-          <Modal animationType="slide" transparent={true} visible={modalVisible} onRequestClose={() => setModalVisible(false)}>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => setModalVisible(false)}
+          >
             <View style={styles.modalContainer}>
               <View style={styles.modalContent}>
                 <Text style={styles.modalTitle}>Add Note</Text>
@@ -304,16 +399,30 @@ console.log("jobId: ", jobId)
                   value={noteContent}
                   onChangeText={setNoteContent}
                 />
-                <TouchableOpacity style={styles.imageButton} onPress={pickImage}>
+                <TouchableOpacity
+                  style={styles.imageButton}
+                  onPress={handleCaptureImage}
+                >
                   <Ionicons name="image-outline" size={24} color="white" />
-                  <Text style={styles.imageButtonText}>Pick an Image</Text>
+                  <Text style={styles.imageButtonText}>Attach Image</Text>
                 </TouchableOpacity>
-                {imageData && <Image source={{ uri: `data:image/png;base64,${imageData}` }} style={styles.previewImage} />}
+                {imageData && (
+                  <Image
+                    source={{ uri: `data:image/png;base64,${imageData}` }}
+                    style={styles.previewImage}
+                  />
+                )}
                 <View style={styles.buttonRow}>
-                  <TouchableOpacity style={styles.cancelNoteButton} onPress={() => setModalVisible(false)}>
+                  <TouchableOpacity
+                    style={styles.cancelNoteButton}
+                    onPress={() => setModalVisible(false)}
+                  >
                     <Text style={styles.cancelNoteButtonText}>Cancel</Text>
                   </TouchableOpacity>
-                  <TouchableOpacity style={styles.addButton} onPress={handleAddNote}>
+                  <TouchableOpacity
+                    style={styles.addButton}
+                    onPress={handleAddNote}
+                  >
                     <Text style={styles.addButtonText}>Add</Text>
                   </TouchableOpacity>
                 </View>
@@ -322,33 +431,37 @@ console.log("jobId: ", jobId)
           </Modal>
         </ScrollView>
       </View>
-    </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   viewStyle: {
     flex: 1,
     backgroundColor: augwaBlue,
+  //  paddingHorizontal: 10
   },
+   scrollContainer: {
+     paddingBottom: 70, // Added padding to the bottom
+ },
   dashboardAreaStyle: {
     marginTop: 20,
-    height: '100%',
+    height: "100%",
     backgroundColor: dashboardArea,
     borderRadius: 30,
   },
   Title: {
     fontSize: 20,
-    color: '#fff',
-    textAlign: 'center',
-    fontWeight: '500',
-    position: 'relative',
-    top: 10
+    color: "#fff",
+    textAlign: "center",
+    fontWeight: "500",
+    position: "relative",
+    top: 10,
   },
   container: {
     flex: 1,
     backgroundColor: "#F8F9FA",
-    padding: 15,
+    paddingBottom: 20,
   },
   header: {
     backgroundColor: "#2D4059",
@@ -419,7 +532,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
     lineHeight: 20,
     marginBottom: 5,
-    marginRight: 16
+    marginRight: 16,
   },
   value: {
     fontSize: 16,
@@ -443,22 +556,15 @@ const styles = StyleSheet.create({
     fontSize: 14,
     marginTop: 5,
   },
-  note: {
-    backgroundColor: "#F1F1F1",
-    padding: 10,
-    borderRadius: 5,
-    marginTop: 5,
-  },
-  noteAuthor: {
+  jobLabel: {
     fontWeight: "bold",
+    fontSize: 14,
+    width: 100, // Fixed width for alignment
   },
-  noteText: {
-    marginTop: 5,
-  },
-  noteDate: {
-    fontSize: 12,
-    color: "#888",
-    marginTop: 3,
+  jobValue: {
+    fontSize: 14,
+    flex: 1,
+    color: "#333",
   },
   arrowIcon: {
     fontSize: 24,
@@ -474,13 +580,7 @@ const styles = StyleSheet.create({
   notesHeader: {
     flexDirection: "row",
     justifyContent: "space-between",
-    alignItems: "center"
-  },
-  noteImage: {
-    width: 200,
-    height: 200,
-    borderRadius: 10,
-    marginTop: 10,
+    alignItems: "center",
   },
   modalContainer: {
     flex: 1,
@@ -513,34 +613,34 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     marginBottom: 10,
   },
-  imageButton: { 
-    flexDirection: "row", 
-    alignItems: "center", 
-    backgroundColor: "#4CD964", 
-    padding: 10, 
-    borderRadius: 5, 
-    marginBottom: 10 
+  imageButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#4CD964",
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
   },
-  imageButtonText: { 
-    color: "#FFF", 
-    marginLeft: 10 
+  imageButtonText: {
+    color: "#FFF",
+    marginLeft: 10,
   },
-  cancelNoteButton: { 
-    backgroundColor: "#FF5A5F", 
-    padding: 10, 
-    borderRadius: 5 
+  cancelNoteButton: {
+    backgroundColor: "#FF5A5F",
+    padding: 10,
+    borderRadius: 5,
   },
-  cancelNoteButtonText: { 
-    color: "#FFF" 
+  cancelNoteButtonText: {
+    color: "#FFF",
   },
-  addButton: { 
-    backgroundColor: "#177de1", 
-    padding: 10, 
-    borderRadius: 5 
+  addButton: {
+    backgroundColor: "#177de1",
+    padding: 10,
+    borderRadius: 5,
   },
-  addButtonText: { 
-    color: "#FFF" 
+  addButtonText: {
+    color: "#FFF",
   },
-})
+});
 
 export default ScheduleDetailScreen;
