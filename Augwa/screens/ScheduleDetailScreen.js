6ea7dddc35a88830b8bdcddc7a7222b70bde855a
ywu256 <ywu256@my.centennialcollege.ca>
2025-03-12 23:43:42 -0400

    Fixed motion detection

--- Augwa/screens/ScheduleDetailScreen.js
+++ Augwa/screens/ScheduleDetailScreen.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, useContext, useCallback } from "react";
+import React, { useEffect, useState, useContext, useCallback } from "react";
 import {
   TouchableOpacity,
   Text,
@@ -266,9 +267,22 @@ const ScheduleDetailScreen = ({ route }) => {
       address: job.address
     });
   };
-  // console.log("address: ",job.latitude)
-  // console.log("address: ",job.longitude)
-  // console.log("address: ",job.address)
+
+  const handleMotionDetected = async () => {
+    if (job?.status === "Scheduled") {
+      try {
+        await api.post(`/Booking/${jobId}/Start`, payload, {
+          headers: { Authorization: `Bearer ${authToken}` },
+        });
+        fetchUpdatedJob();
+        console.log("Job marked as En Route");
+      } catch (error) {
+        console.error("Failed to update job status:", error);
+      }
+    }
+  };
+
+  useMotionDetection(handleMotionDetected);
 
   return (
     <SafeAreaView style={styles.viewStyle}>
